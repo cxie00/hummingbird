3641ddcb839a1666c4e3896e3695483b83380e1d
Chloe Xie <t-xiechloe@microsoft.com>
2022-06-23 15:21:01 -0700

    Committed job script

--- notebooks/scripts/blog_example.py
+++ notebooks/scripts/blog_example.py
@@ -2,15 +2,21 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_breast_cancer
 from hummingbird.ml import convert
 from sklearn.metrics import accuracy_score
+import mlflow
+import time
+
+mlflow.start_run()
 
 # Create and train a RandomForestClassifier model
 X, y = load_breast_cancer(return_X_y=True)
 skl_model = RandomForestClassifier(n_estimators=500, max_depth=7)
 skl_model.fit(X, y)
 
-import time
-# file1 = open("metrics/blog_example_metrics.txt", "w") 
+# log the model
+mlflow.sklearn.log_model(skl_model, "sk_models")
+print(type(skl_model))
 
+# pre-conversion: time predict() with RandomForestClassifier and get accuracy score
 pre_conv = []
 pre_acc = []
 n = 1
@@ -24,13 +30,19 @@ for i in range(0, n):
 pre_avg_ms = sum(pre_conv) / n * 1000
 pre_acc_avg = sum(pre_acc) / n 
 pre_metric = f"Pre-conversion pred average: {pre_avg_ms:.2f} ms over {n} runs \n Pre-conversion accuracy average: {pre_acc_avg:.5f} \n"
-# file1.write(pre_metric)
-print("\n")
 print(pre_metric)
-print("\n")
 
+# pre-conversion: log metrics
+mlflow.log_metric('Pre-conversion-prediction-time (ms)', pre_avg_ms) 
+mlflow.log_metric('Pre-conversion-accuracy-score', pre_acc_avg)
+
+# convert the model
 model = convert(skl_model, 'torch')
 
+# log the pytorch model
+mlflow.sklearn.log_model(model, 'torch_model')
+
+# post conversion: time predict() with pytorch model and get accuracy score
 post_conv = []
 post_acc = []
 for i in range(0, n):
@@ -43,43 +55,31 @@ for i in range(0, n):
 post_avg_ms = sum(post_conv) / n * 1000
 post_acc_avg = sum(post_acc) / n 
 post_metric = f"Post-conversion average: {post_avg_ms:.2f} ms over {n} runs \n Post-conversion accuracy average: {post_acc_avg:.5f} \n"
-# file1.write(post_metric)
-print("\n")
 print(post_metric)
-print("\n")
+print(type(model))
+model.to('cuda')
 
-# model.to('cuda')
+# post converion: log metrics
+mlflow.log_metric('Post-conversion prediction time (ms)', post_avg_ms)
+mlflow.log_metric('Post-conversion accuracy score', post_acc_avg)
 
-# post_gpu_conv = []
-# post_gpu_acc = []
-# for i in range(0, n):
-#     start = time.perf_counter()
-#     pred_gpu_hb = model.predict(X)
-#     end = time.perf_counter()
-#     post_gpu_conv.append(end - start)
-#     post_gpu_accuracy = accuracy_score(y, pred_gpu_hb)
-#     post_gpu_acc.append(post_gpu_accuracy)
-# post_gpu_avg_ms = sum(post_gpu_conv) / n * 1000
-# post_acc_gpu_avg = sum(post_gpu_acc) / n
-# post_gpu_metric = f"Post-conversion GPU average: {post_gpu_avg_ms:.2f} ms over {n} runs \n Post-conversion GPU accuracy average: {post_acc_gpu_avg:.5f} \n"
-# file1.write(post_gpu_metric)
+# GPU: time predict() and get accuracy score
+post_gpu_conv = []
+post_gpu_acc = []
+for i in range(0, n):
+    start = time.perf_counter()
+    pred_gpu_hb = model.predict(X)
+    end = time.perf_counter()
+    post_gpu_conv.append(end - start)
+    post_gpu_accuracy = accuracy_score(y, pred_gpu_hb)
+    post_gpu_acc.append(post_gpu_accuracy)
+post_gpu_avg_ms = sum(post_gpu_conv) / n * 1000
+post_acc_gpu_avg = sum(post_gpu_acc) / n
+post_gpu_metric = f"Post-conversion GPU average: {post_gpu_avg_ms:.2f} ms over {n} runs \n Post-conversion GPU accuracy average: {post_acc_gpu_avg:.5f} \n"
+print(post_gpu_metric)
 
-# file1.close()
-
-# file1 = open("metrics/blog_example_pre_conv.txt", "w") 
-# for t in pre_conv:
-#     file1.write(f"{t}\n")
-
-# file1.close()
-
-# file1 = open("metrics/blog_example_post_conv.txt", "w") 
-# for t in post_conv:
-#     file1.write(f"{t}\n")
-
-# file1.close()
-
-# file1 = open("metrics/blog_example_post_conv_gpu.txt", "w") 
-# for t in post_gpu_conv:
-#     file1.write(f"{t}\n")
-
-# file1.close()
+# GPU: log metrics
+mlflow.log_metric('GPU post-conversion prediction time (ms)', post_gpu_avg_ms)
+mlflow.log_metric('GPU post-conversion accuracy score', post_acc_gpu_avg)
+print(type(model.model))
+mlflow.end_run()
